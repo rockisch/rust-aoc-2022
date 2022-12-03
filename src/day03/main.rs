a54beffbcb2d7f48edbc89d25c5992dcf247617f
rockisch <rockischplay@gmail.com>
2022-12-03 12:28:21 -0300

    Reimplement using bitsets

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -1,60 +1,44 @@
 #![feature(test)]
 #![feature(iter_array_chunks)]
-#![feature(hash_drain_filter)]
-use std::{collections::{HashMap, HashSet}, ops::Index};
 extern crate test;
 
 pub const EXAMPLE: &[u8] = include_bytes!("example.txt");
 pub const DATA: &[u8] = include_bytes!("data.txt");
 
 fn main() {
-    let r1 = solve_1(DATA);
+    let r1 = solve_1(EXAMPLE);
     println!("Part 1: {r1}");
     let r2 = solve_2(DATA);
     println!("Part 2: {r2}");
 }
 
 fn solve_1(input: &[u8]) -> String {
-    let mut hash = HashSet::<u8>::new();
     input.split(|&v| v == b'\n')
         .filter(|v| !v.is_empty())
         .map(|v| {
             let (b1, b2) = v.split_at(v.len()/2);
-            hash.clear();
-            hash.extend(b1);
-            let r = b2.iter().find(|v| hash.contains(v)).unwrap();
-            if r.is_ascii_uppercase() {
-                (r - b'A' + 27) as u16
-            } else {
-                (r - b'a' + 1) as u16
+            let (mut bit1, mut bit2): (u64, u64) = (0, 0);
+            for (v1, v2) in b1.into_iter().zip(b2.into_iter()) {
+                bit1 |= 1 << (v1 - 64);
+                bit2 |= 1 << (v2 - 64);
             }
+            let r = ((bit1 & bit2).trailing_zeros() + 64) as u8;
+            (if r >= 97 { r - b'a' + 1 } else { r - b'A' + 27 }) as u16
         })
         .sum::<u16>()
         .to_string()
 }
 
 fn solve_2(input: &[u8]) -> String {
-    let mut hash = HashSet::<u8>::new();
-    let mut hash_aux = HashSet::<u8>::new();
     input.split(|&v| v == b'\n')
         .filter(|v| !v.is_empty())
         .array_chunks::<3>()
-        .map(|v| {
-            let [b1, b2, b3] = v;
-            hash.clear();
-            hash.extend(b1);
-            hash_aux.clear();
-            hash_aux.extend(b2);
-            hash.drain_filter(|v| !hash_aux.contains(v));
-            hash_aux.clear();
-            hash_aux.extend(b3);
-            hash.drain_filter(|v| !hash_aux.contains(v));
-            let r = hash.iter().next().unwrap();
-            if r.is_ascii_uppercase() {
-                (r - b'A' + 27) as u16
-            } else {
-                (r - b'a' + 1) as u16
-            }
+        .map(|[b1, b2, b3]| {
+            let bit1: u64 = b1.into_iter().fold(0, |acc, &v| acc | (1 << (v - 64)));
+            let bit2: u64 = b2.into_iter().fold(0, |acc, &v| acc | (1 << (v - 64)));
+            let bit3: u64 = b3.into_iter().fold(0, |acc, &v| acc | (1 << (v - 64)));
+            let r = ((bit1 & bit2 & bit3).trailing_zeros() + 64) as u8;
+            (if r >= 97 { r - b'a' + 1 } else { r - b'A' + 27 }) as u16
         })
         .sum::<u16>()
         .to_string()
@@ -70,7 +54,7 @@ mod tests {
         assert_eq!(solve_1(EXAMPLE), "157");
         assert_eq!(solve_1(DATA), "7674");
         assert_eq!(solve_2(EXAMPLE), "70");
-        // assert_eq!(solve_2(DATA), "");
+        assert_eq!(solve_2(DATA), "2805");
     }
 
     #[bench]
